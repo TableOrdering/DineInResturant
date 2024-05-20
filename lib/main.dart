adf2b1fab7f167ebb7d9445f98bb4adb84efa0d2
Arbaan Qureshi S <133197675+Arbaan-royalMart@users.noreply.github.com>
2024-05-20 10:39:53 +0530

    use full commands added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
-
 import 'package:dine_in_resturant/app/start.dart';
 
 import 'app/app.dart';
 
 /// [main] is the entry point of the application.
 Future<void> main() async => await startApplication(() => const Application());
+/*
+/// Release Command with hidden Packages 
+// flutter build apk --split-per-abi --split-debug-info=path/to/mapping/directory --obfuscate --release
+
+/// Relase Build Without Hidding Package
+// flutter build apk --split-per-abi
+
+/// Chrome Build and Release Things 
+// flutter run -d chrome --web-port=6969  --web-browser-flag "--disable-web-security"
+ flutter build web --web-renderer html --release
+ firebase init hosting
+ "target": "ecomcrm",
+ firebase target:apply hosting ecomcrm ecomcrm
+ firebase deploy --only hosting:ecomcrm 
+*/  
