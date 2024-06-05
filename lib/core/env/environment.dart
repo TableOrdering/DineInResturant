4789c2f76287b2fdf2bba923fb40e3e0f5a2a81e
Arbaan Qureshi S <102940891+arbaan-max@users.noreply.github.com>
2024-06-05 12:46:22 +0530

    changed the URL of AWS

--- lib/core/env/environment.dart
+++ lib/core/env/environment.dart
@@ -5,8 +5,8 @@ const kAPIVersion = '/v1';
 /// [kBaseURL] is base url for api calls.
 // const kBaseURL = 'dine-in-backend.onrender.com';
 // http://localhost:5300/api/v1/resturant
-const kBaseURL = 'localhost:5300';
-// const kBaseURL = '192.168.45.42:5300';
+// const kBaseURL = 'localhost:5300';
+const kBaseURL = 'ec2-15-207-79-26.ap-south-1.compute.amazonaws.com';
 
 /// [kMinimumBuildNumber] is the minimum build number required for the app to work.
 /// [X-Client-Headers] is a custom header that is used to identify the client.
